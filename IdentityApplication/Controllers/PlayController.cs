4a83ebcc8c7322a322ce0638cbc8f6ecc46965e3
Ahmed Gamal <gemi6276@gmail.com>
2024-05-16 11:49:49 +0200

    Add Play Controller

---
+++ IdentityApplication/Controllers/PlayController.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Messaging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApplication.API.Controllers
+{
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayController : ControllerBase
+    {
+        [HttpGet("get-players")]
+        public IActionResult Play()
+        {
+
+            return Ok(new JsonResult(new {message = "Only authorize users can view playres" } ));
+        }
+
+    }
+}
